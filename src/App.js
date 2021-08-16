3ab4669a1c9e9dbbc2609b6b3c1ebbebb5209170
Jack <jackpar1113@gmail.com>
2021-08-16 17:02:08 -0600

    layout basic structure of website with h3 tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Work in progress</p>
+      <h1>This website is currently being built</h1>
+      <h3>Navbar</h3>
+
+      <h3>Intro Page</h3>
+
+      <h3>About Me Page</h3>
+
+      <h3>Personal Project 1</h3>
+
+      <h3>Personal Project 2</h3>
+
+      <h3>Personal Project 3</h3>
+
+      <h3>Contact Me</h3>
+
+      <h3>Footer</h3>
+      
     </div>
   );
 }
