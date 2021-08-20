17ea16029d0e7010f0755a5072747df60039d0fd
Jack <jackpar1113@gmail.com>
2021-08-20 17:19:28 -0600

    add waves as transitions between each section

--- src/Components/About Me Page/About.js
+++ src/Components/About Me Page/About.js
@@ -1,9 +1,11 @@
+import LightWave from "../Waves/LightWave";
 import "./About.css";
 
 function About() {
   return (
     <section id="about" className="about">
       <h1>This is the about me page</h1>
+      <LightWave />
     </section>
   );
 }
--- src/Components/LandingPage/Landing.js
+++ src/Components/LandingPage/Landing.js
@@ -1,9 +1,11 @@
+import DarkWave from "../Waves/DarkWave";
 import "./Landing.css";
 
 function Landing() {
   return (
     <section id="landing" className="landing">
       <h1>This is a landing page</h1>
+      <DarkWave />
     </section>
   );
 }
