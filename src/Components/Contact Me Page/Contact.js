08dfad3bb6c0699ce7686dbf9535d4e342966315
Jack <jackpar1113@gmail.com>
2021-08-25 16:33:53 -0600

    add contact me page

--- src/Components/Contact Me Page/Contact.js
+++ src/Components/Contact Me Page/Contact.js
@@ -1,10 +1,34 @@
 import LightWave from "../Waves/LightWave";
 import "./Contact.css";
+import { FaLinkedin, FaMailBulk } from "react-icons/fa";
 
 function Contact() {
   return (
     <section id="contact" className="contact">
-      <h2>Contact Me</h2>
+      <div className="contact__body">
+        <h2>Contact Me</h2>
+        <p className="contact__p">
+          Currently I am looking from a summer 2022 Software Development
+          Internship
+        </p>
+        <div className="contact__option">
+          <FaMailBulk className="contact__logo" size={45} />
+          <a href="mailto: jparsons@ualberta.ca">
+            <span className="contact__text">jparsons@ualberta.ca</span>
+          </a>
+        </div>
+        <div className="contact__option">
+          <FaLinkedin className="contact__logo" size={45} />
+          <a
+            href="https://www.linkedin.com/in/jack-parsonss/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="my Linkedin"
+          >
+            <span className="contact__text">jack-parsons</span>
+          </a>
+        </div>
+      </div>
       <LightWave />
     </section>
   );
