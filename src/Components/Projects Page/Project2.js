269ab38a3da20103f020805b73e5e363a9c6f5fb
Jack <jackpar1113@gmail.com>
2021-08-25 20:49:27 -0600

    add gradients to every background

---
+++ src/Components/Projects Page/Project2.js
@@ -0,0 +1,13 @@
+import LightWave from "../Waves/LightWave";
+import "./Project.css";
+
+function Project2() {
+  return (
+    <article className="project dark">
+      <h3>This is project 3</h3>
+      <LightWave />
+    </article>
+  );
+}
+
+export default Project2;
