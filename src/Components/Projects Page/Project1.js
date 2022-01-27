46af6b336beda68d98977ae36152f6dc5f5cfaa0
Jack <jackpar1113@gmail.com>
2022-01-27 12:40:20 -0700

    update resume, update projects to remove pacman game and add health fusion

--- src/Components/Projects Page/Project1.js
+++ src/Components/Projects Page/Project1.js
@@ -1,39 +1,74 @@
 import { FaGithub } from "react-icons/fa";
 import DarkWave from "../Waves/DarkWave";
 import "./ProjectLight.css";
+import MediaQuery from "react-responsive";
+import { useMediaQuery } from "react-responsive";
 
 function Project1({ id }) {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 769px)",
+  });
+
   return (
     <article id={id} className="project light">
-      <h2 className="project__header">Projects</h2>
       <div className="project__main">
         <img
-          className="project__image"
-          src="/images/flavour_flow_image_large.png"
-          srcset="/images/flavour_flow_image_large.png 750w, /images/flavour_flow_image_large.png 1468w"
-          sizes="50vw"
-          alt="Flavout Flow Landing Page"
-          width="50%"
+          className="project__image project__specialMobileImage"
+          src="/images/health_fusion_home.png"
+          alt="Health Fusion Project Home Page"
+          width="15%"
           height="auto"
           loading="lazy"
         />
+        {isDesktopOrLaptop ? (
+          <>
+            <img
+              className="project__image"
+              src="/images/health_fusion_add.png"
+              alt="Health Fusion Map Page"
+              width="15%"
+              height="auto"
+              loading="lazy"
+            />
+            <img
+              className="project__image"
+              src="/images/health_fusion_map.png"
+              alt="Health Fusion Project Add Page"
+              width="15%"
+              height="auto"
+              loading="lazy"
+            />
+          </>
+        ) : (
+          <> </>
+        )}
+
         <div data-aos="fade-left" className="project__right">
-          <h3 className="project__title">Flavour Flow</h3>
+          <h3 className="project__title">Health Fusion</h3>
           <p className="project__description project__body">
-            A <span className="project__specialText">hackathon</span> project
-            for Hack The North 2021. Allows users to{" "}
-            <span className="project__specialText">enter ingredients</span> and{" "}
-            <span className="project__specialText">get recipes</span>. The
-            recipe generator maximizes given ingredients and minimizes any
-            missing ingredients, using the{" "}
-            <span className="project__specialText">Spoonacular API</span>.
+            A <span className="project__specialText">mobile app</span> built to
+            help manage life. Allows users to{" "}
+            <span className="project__specialText">
+              manage all of their medications
+            </span>
+            , with an intuitive add button combined with a simple home screen to
+            checkoff all medications. An enjoyable{" "}
+            <span className="project__specialText">calendar view</span> is
+            available for all users so they can view their{" "}
+            <span className="project__specialText">medication history</span>
+            from the past or look to the future and see what they need to take.
+            Finally, users are also given a{" "}
+            <span className="project__specialText">map view</span> so that they
+            have no problem finding any nearby pharmacies or hostiptals if
+            needed.
           </p>
           <p className="project__tech project__body">
-            &gt; Django · React · CSS · CockroachDB · Spoonacular API
+            &gt; React Native · Firebase · Firestore · Google Maps API · Google
+            Autocomplete API
           </p>
           <a
             className="project__sourceCode"
-            href="https://github.com/jackparsonss/Flavour-Flow"
+            href="https://github.com/jackparsonss/health-fusion"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="=Website GitHub Repository"
--- src/Components/Projects Page/Project3.js
+++ src/Components/Projects Page/Project3.js
@@ -5,34 +5,35 @@ import "./ProjectLight.css";
 function Project3() {
   return (
     <article className="project light">
+      <h2 className="project__header">Projects</h2>
       <div className="project__main">
         <img
           className="project__image"
-          src="/images/pacman_project_image_large.jpg"
-          srcset="/images/pacman_project_image_small.jpg 900w, /images/pacman_project_image_large.jpg 1800w"
+          src="/images/flavour_flow_image_large.png"
+          srcset="/images/flavour_flow_image_large.png 750w, /images/flavour_flow_image_large.png 1468w"
           sizes="50vw"
-          alt="Covid Bot Project Commands"
+          alt="Flavout Flow Landing Page"
           width="50%"
           height="auto"
           loading="lazy"
         />
         <div data-aos="fade-left" className="project__right">
-          <h3 className="project__title">3D Pacman</h3>
+          <h3 className="project__title">Flavour Flow</h3>
           <p className="project__description project__body">
-            A <span className="project__specialText">3D video game</span> based
-            on the classic retro game Pacman. However, there are twists, such as
-            a <span className="project__specialText">physics</span> based
-            movement system. All ghosts are using a{" "}
-            <span className="project__specialText">finite state machine</span>{" "}
-            and the <span className="project__specialText">A* algorithm</span>{" "}
-            to hunt.
+            A <span className="project__specialText">hackathon</span> project
+            for Hack The North 2021. Allows users to{" "}
+            <span className="project__specialText">enter ingredients</span> and{" "}
+            <span className="project__specialText">get recipes</span>. The
+            recipe generator maximizes given ingredients and minimizes any
+            missing ingredients, using the{" "}
+            <span className="project__specialText">Spoonacular API</span>.
           </p>
           <p className="project__tech project__body">
-            &gt; C# · Unity Game Engine
+            &gt; Django · React · CSS · CockroachDB · Spoonacular API
           </p>
           <a
             className="project__sourceCode"
-            href="https://github.com/jackparsonss/3D-Pacman"
+            href="https://github.com/jackparsonss/Flavour-Flow"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="=Website GitHub Repository"
